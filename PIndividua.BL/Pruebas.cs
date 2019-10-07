3f92020290363009540cf3ed1d9524bb821c1bd7
Jeison badilla <j.jeison94@hotmail.com>
2019-10-07 13:46:35 -0600

    Add project files.

---
+++ PIndividua.BL/Pruebas.cs
@@ -0,0 +1,38 @@
+﻿using PIndividual.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIndividual.BL
+{
+    public class Pruebas
+    {
+        public void AddVuelo(Vuelos v1)
+        {
+            using (AeropuertoEntities context = new AeropuertoEntities())
+            {
+
+                context.Vuelos.Add(v1);
+                context.SaveChanges();
+            }
+        }
+        public void AddAvion(Aviones A1)
+        {
+            using (AeropuertoEntities context = new AeropuertoEntities())
+            {
+                context.Aviones.Add(A1);
+                context.SaveChanges();
+            }
+        }
+        public void AddPasajero(Pasajeros P1)
+        {
+            using (AeropuertoEntities context = new AeropuertoEntities())
+            {
+                context.Pasajeros.Add(P1);
+                context.SaveChanges();
+            }
+        }
+    }
+}
